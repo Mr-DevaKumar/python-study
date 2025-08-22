a986e238a6335a2ee88d72454d3af70bd7c52de7
devakumar <deva29008@gmail.com>
2025-08-22 13:57:28 +0530

    update leap year program

--- leap_year.py
+++ leap_year.py
@@ -17,8 +17,27 @@ def leap_year_if_else(year):
 # Build-in
 
 def leap_year_build_in(year):
-    import calender
+    import calender # type: ignore
     return calender.isleap(year)
 
 def leapYear():
-    
+    print("Leap Year Calculation")
+    while True:
+        try:
+            year = input("Enter a year (or 'quit' to exit):")
+
+            if year.lower() == 'quit':
+                print("Thank You")
+                break
+
+            year = int(year)
+
+            print(leap_year_if_else(year))
+            print(leap_year_build_in(year))
+
+        except ValueError:
+            print("Error: Please enter a valid year")
+        except Exception as e:
+            print("An error occured ", e)
+
+leapYear()
